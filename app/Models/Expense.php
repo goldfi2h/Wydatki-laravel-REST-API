e326f81f4c77f0c9f47543c29bc432e7d8b98ef8
Natalia <natwisnie@wp.pl>
2023-02-28 09:55:19 +0100

    Expenses and Category relation?

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Expense extends Model
 {
@@ -15,4 +16,9 @@ class Expense extends Model
         'user_id',
         'category_id'
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
